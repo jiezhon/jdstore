37055db3264322d66113a4cab85cfdc00ed084ce
zhongjie <zhongjie10@gmail.com>
2017-03-05 14:34:41 +0800

    update quantity of dishes after ordered

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,7 @@ class OrdersController < ApplicationController
     @order.user = current_user
     @order.total = current_cart.total_price
     @order.book_date = current_cart.book_date
-    
+
     if @order.save
       current_cart.cart_items.each do |cart_item|
         if !cart_item.product.is_hidden
@@ -17,6 +17,11 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.style = cart_item.product.style
         product_list.save
+
+        product = cart_item.product
+        origin_quantity = product.quantity
+        product.quantity = origin_quantity - cart_item.quantity
+        product.save
         end
       end
 
@@ -29,10 +34,12 @@ class OrdersController < ApplicationController
       @chef_shadow.phone = chef.phone
       @chef_shadow.save
 
+      #clean current cart
       current_cart.chef_id = nil
       current_cart.book_date = nil
       current_cart.save
       current_cart.clean!
+
 
       OrderMailer.notify_order_placed(@order, @chef_shadow).deliver!
 
@@ -43,7 +50,7 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find_by_token(params[:id])
+    @order = current_user.orders.find_by_token(params[:id])
     @product_lists = @order.product_lists
     @chef_shadow = ChefShadow.find_by(order_id: @order.id)
   end
